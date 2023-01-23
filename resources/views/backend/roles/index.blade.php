40896edfca475d7ba7342e497cecd879376c308e
Polash Mahmud <polashmahmud@gmail.com>
2023-01-23 23:52:47 +0600

    working on role

---
+++ resources/views/backend/roles/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.backend.app')
+
+@section('content')
+    <div class="container-xl">
+        <!-- Page title -->
+        <div class="page-header d-print-none">
+            <div class="row align-items-center">
+                <div class="col">
+                    <h2 class="page-title">
+                        Empty page
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="page-body">
+        <div class="container-xl">
+
+        </div>
+    </div>
+@endsection
