e5b249c31e6c7e89edc0b28d909a9ade5622b790
Polash Mahmud <polashmahmud@gmail.com>
2023-01-29 13:16:42 +0600

    add filter

--- app/Http/Livewire/Backend/RoleDataTable.php
+++ app/Http/Livewire/Backend/RoleDataTable.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Livewire\Backend;
 
+use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\Role;
 use Rappasoft\LaravelLivewireTables\Views\Columns\ButtonGroupColumn;
 use Rappasoft\LaravelLivewireTables\Views\Columns\LinkColumn;
+use Rappasoft\LaravelLivewireTables\Views\Filters\SelectFilter;
 
 class RoleDataTable extends DataTableComponent
 {
@@ -15,6 +17,42 @@ class RoleDataTable extends DataTableComponent
     public function configure(): void
     {
         $this->setPrimaryKey('id');
+        $this->setBulkActions([
+            'exportSelected' => 'Export',
+            'deleteSelected' => 'Delete',
+        ]);
+        $this->setHideBulkActionsWhenEmptyStatus(true);
+    }
+
+    public function exportSelected()
+    {
+        dd($this->getSelected());
+//        $this->clearSelected();
+    }
+
+    public function deleteSelected()
+    {
+        dd($this->getSelected());
+//        $this->clearSelected();
+    }
+
+    public function filters(): array
+    {
+        return [
+            SelectFilter::make('Deletable')
+                ->options([
+                    '' => 'All',
+                    '1' => 'Yes',
+                    '0' => 'No',
+                ])
+                ->filter(function(Builder $builder, string $value) {
+                    if ($value === '1') {
+                        $builder->where('deletable', true);
+                    } elseif ($value === '0') {
+                        $builder->where('deletable', false);
+                    }
+                }),
+        ];
     }
 
     public function columns(): array
