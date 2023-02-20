b81a4ba91788cf2f35f9c25cb7a75d735e67af07
Polash Mahmud <polashmahmud@gmail.com>
2023-02-20 08:53:51 +0600

    working on crud

--- app/Http/Livewire/Settings/Crud/CrudForm.php
+++ app/Http/Livewire/Settings/Crud/CrudForm.php
@@ -23,9 +23,9 @@ class CrudForm extends Component
     ];
 
     protected $rules = [
-        'model'               => 'required|string|max:255',
-        'folder'              => 'required|string|max:255',
-        'controller'          => 'required|string|max:255',
+        'model'      => 'required|string|max:255',
+        'folder'     => 'required|string|max:255',
+        'controller' => 'required|string|max:255',
 //        'fields'              => 'required|array|min:1',
 //        'fields.*.name'       => 'required|string|max:255',
 //        'fields.*.type'       => 'required|string|max:255',
@@ -77,22 +77,22 @@ class CrudForm extends Component
         $validatedData = $this->validate();
 
         // check model is exists or not
-        if (class_exists('App\\Models\\' . $this->model)) {
-            $this->addError('model', 'Model already exists');
-            return;
-        }
+//        if (class_exists('App\\Models\\' . $this->model)) {
+//            $this->addError('model', 'Model already exists');
+//            return;
+//        }
 
         // check views folder is exists or not
-        if (is_dir(resource_path('views/' . $this->folder))) {
-            $this->addError('folder', 'Folder already exists');
-            return;
-        }
+//        if (is_dir(resource_path('views/' . $this->folder))) {
+//            $this->addError('folder', 'Folder already exists');
+//            return;
+//        }
 
         // check controller is exists or not
-        if (class_exists('App\\Http\\Controllers\\' . $this->controller)) {
-            $this->addError('controller', 'Controller already exists');
-            return;
-        }
+//        if (class_exists('App\\Http\\Controllers\\' . $this->controller)) {
+//            $this->addError('controller', 'Controller already exists');
+//            return;
+//        }
 
         // create model, controller, views, migration
 
@@ -101,7 +101,37 @@ class CrudForm extends Component
 //        Artisan::call('make:controller ' . $this->controller . ' --resource');
 
         // make a folder in views
-        mkdir(resource_path('views/' . $this->folder));
+//        mkdir(resource_path('views/' . $this->folder . '/'));
+
+        // make index view
+//        $indexView = fopen(resource_path('views/' . $this->folder . '/index.blade.php'), 'w');
+//        fwrite($indexView, 'index view');
+//        fclose($indexView);
+
+        // copy create and edit view
+//        copy(resource_path('views/settings/crud/files/index.blade.php'), resource_path('views/' . $this->folder . '/index.blade.php'));
+
+        // open views/bac/index.blade.php if there exits [one], [two], [three] then replace with 1, 2, 3
+        $indexView = fopen(resource_path('views/bac/index.blade.php'), 'r');
+        $indexViewContent = fread($indexView, filesize(resource_path('views/bac/index.blade.php')));
+        fclose($indexView);
+
+        // make a array of [one], [two], [three] and replace with 1, 2, 3, using foreach loop
+
+        $array = ['[one]', '[two]', '[three]'];
+        $replace = ['1', '2', '3'];
+
+        $indexViewContent = str_replace($array, $replace, $indexViewContent);
+
+
+//        $indexViewContent = str_replace('[one]', '1', $indexViewContent);
+//        $indexViewContent = str_replace('[two]', '2', $indexViewContent);
+//        $indexViewContent = str_replace('[three]', '3', $indexViewContent);
+
+        $indexView = fopen(resource_path('views/bac/index.blade.php'), 'w');
+        fwrite($indexView, $indexViewContent);
+        fclose($indexView);
+
 
         dd($validatedData);
     }
---
+++ resources/views/bac/index.blade.php
@@ -0,0 +1,8 @@
+@extends('settings.layouts.default')
+
+@section('settings-content')
+    1
+    2
+    3
+    [four]
+@endsection
